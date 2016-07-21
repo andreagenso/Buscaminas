39283e826a638bd546805ed9985d8f4b9cd15c86
Cherry2 <Cherry2>
2016-07-21 14:04:52 -0400

    agregar Principal basic

---
+++ src/Principal.java
@@ -0,0 +1,82 @@
+import buscaminas.Buscaminas;
+import buscaminas.NivelJuego;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		
+		Buscaminas buscaminas;
+		
+		// interaccion con el teclado
+		@SuppressWarnings("resource")
+		java.util.Scanner in = new java.util.Scanner(System.in);		
+		String opcionStr;
+		
+				
+		boolean continuarJuego = true;
+		boolean jugar = true;
+				
+		System.out.println("BIENVENIDO AL JUEGO BUSCAMINAS * * *");
+		while(continuarJuego){									
+			System.out.println("POR FAVOR ELIJA UNA DE LAS OPCIONES: ");
+			System.out.println("\t B) Para elegir el nivel BASICO de juego");
+			System.out.println("\t M) Para elegir el nivel MEDIO de juego");
+			System.out.println("\t A) Para elegir el nivel AVANZADO de juego");
+			System.out.println("\t E) Para empezar el juego");
+			System.out.println("\t Q) Para salir del juego");			
+						
+			opcionStr = in.next();
+			switch (opcionStr) {
+			case "B" :
+				buscaminas = new Buscaminas(NivelJuego.BASICO);
+				buscaminas.mostrarJugada();
+				
+				System.out.println("INICIANDO JUEGO .... ");
+				System.out.println("\t ingrese un numero para elegir una fila como posicion X, "
+						+ "posteriormente otro número para elgir la columna como posición y");
+				System.out.println("\t Q) Para salir del juego.");
+								
+				while(jugar){
+					buscaminas.jugar(jugar);									
+				}																								
+				break;
+			case "M" :
+				buscaminas = new Buscaminas(NivelJuego.MEDIO);
+				buscaminas.mostrarJugada();
+				
+				System.out.println("INICIANDO JUEGO .... bla bla instrucciones para jugo MEDIO ");
+				System.out.println("\t ingrese un numero para elegir una fila como posicion X, "
+						+ "posteriormente otro número para elgir la columna como posición y");
+				System.out.println("\t Q) Para salir del juego.");
+								
+				while(jugar){
+					buscaminas.jugar(jugar);									
+				}																								
+				break;				
+			case "A" :
+				buscaminas = new Buscaminas(NivelJuego.BASICO);
+				buscaminas.mostrarJugada();
+				
+				System.out.println("INICIANDO JUEGO .... bla bla instrucciones para juego AVANZADO ");
+				System.out.println("\t ingrese un numero para elegir una fila como posicion X, "
+						+ "posteriormente otro número para elgir la columna como posición y");
+				System.out.println("\t Q) Para salir del juego.");
+								
+				while(jugar){
+					buscaminas.jugar(jugar);									
+				}																								
+				break;						
+			case "Q" :
+				System.out.println("\t ADIOS ... ");
+				continuarJuego = false;
+				break;			
+			default :	
+				System.out.println("\tOPCION DESCONOCIDA, POR FAVOR ELIJA UNA OPCION NUEVAMENTE!");
+				break;
+			}
+		}		
+
+		
+	}
+
+}
--- src/buscaminas/Buscaminas.java
+++ src/buscaminas/Buscaminas.java
@@ -9,17 +9,42 @@ public class Buscaminas {
 		
 		switch (this.nivel) {
 			case BASICO:
-				matriz = new Matriz(4);
+				this.matriz = new Matriz(4);
 			case MEDIO:
-				matriz = new Matriz(15);
+				this.matriz = new Matriz(15);
 			case AVANZADO:
-				matriz = new Matriz(40);
+				this.matriz = new Matriz(40);
 			default:	
-				matriz = new Matriz(4);
-		}		
-			
+				this.matriz = new Matriz(4);
+		}					
 	}
 	
+	public Celda[][] reiniciarJuego(NivelJuego nivel){
+		switch (nivel) {
+		case BASICO:
+			this.matriz = new Matriz(4);
+		case MEDIO:
+			this.matriz = new Matriz(15);
+		case AVANZADO:
+			this.matriz = new Matriz(40);
+		default:	
+			this.matriz = new Matriz(4);
+		}
+		
+		return this.matriz.getCeldas();
+	}
+	
+	public void mostrarJugada(){
+		Celda[][] celdas = matriz.getCeldas();
+				
+		for (int i = 0; i < celdas.length; i++) {
+		    for (int j = 0; j < celdas[i].length; j++) {
+		        System.out.print("| " + celdas[i][j] + " |");
+		    }
+		    System.out.println();
+		}
+	}	
+		
 	public Celda[][] jugarPosicion(Posicion posicion) {
 		if (validarPosicion(posicion)) {			
 			matriz.hacerVisible(posicion);					
@@ -40,6 +65,48 @@ public class Buscaminas {
 		int j = posicion.y;
 		
 		return (i >= 0 && i <= matriz.getTam() -1) && (j >= 0 && j <= matriz.getTam() -1);				
+	}
+	
+	public void jugar(boolean continuarJuego){
+		System.out.println("\t Ingrese posición X");
+		
+		@SuppressWarnings("resource")
+		java.util.Scanner in = new java.util.Scanner(System.in);
+		String posicionXStr;
+		String posicionYStr;
+		int posicionX;
+		int posicionY;
+				
+		posicionXStr = in.next();		
+		try{
+			if (posicionXStr == "Q") {
+				continuarJuego = false;
+				return;
+			}
+			posicionX = Integer.parseInt(posicionXStr);
+		} catch (java.util.InputMismatchException m){ posicionX =  -1;}
+		catch (Exception e){ posicionX =  -1;}
+		
+		System.out.println("\t Ingrese posición Y");
+		posicionYStr = in.next();
+		try{
+			if (posicionYStr == "Q") {
+				continuarJuego = false;
+				return;
+			}
+			posicionY = Integer.parseInt(posicionXStr);
+		}
+		catch (java.util.InputMismatchException m){ posicionY =  -1;}
+		catch (Exception e){ posicionY =  -1;}
+		
+		Posicion posicion = new Posicion(posicionX, posicionY);
+		jugarPosicion(posicion);
+		
+		if (esJuegoGanado()){
+			System.out.println("GANASTE!");
+		} else if (esJuegoPerdido()) {
+			System.out.println("PERDISTE!");
+		}		
 	}
 		
 }
